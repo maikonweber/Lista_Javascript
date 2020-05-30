b5413812f1a80272fac55d20bd0f8a1401a138dc
Maikon Weber <maikonweber@gmail.com>
2020-05-30 20:56:00 -0300

    Adicionando Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Pagina em Html
+
+Que possue uma lista e ela é renderizado via javascript  vocẽ pde adicionar elementos nesta lista como se fosse uma lista de tarefas e salvar na localStorage.
--- js.js
+++ js.js
@@ -6,7 +6,7 @@ var buttonElement = document.querySelector ("#app button");
 
 
 
-var todos = JSON.parse(localStorage.getItem("list_todos"));
+var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
 function renderTodos() {
     listElement.innerHTML = "";
     for(todo of todos) {

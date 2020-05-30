017dc866a288eae63fd7974fa04feecedbf91405
Maikon Weber <maikonweber@gmail.com>
2020-05-30 20:32:20 -0300

    Aprendendo Javascript

---
+++ js.js
@@ -0,0 +1,45 @@
+
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector ("#app input");
+var buttonElement = document.querySelector ("#app button");
+
+var todos = ["Fazer Cafe",
+    "Estudar Javascript",
+    "Acessar a Comunidade"
+];
+
+function renderTodos() {
+    listElement.innerHTML = "";
+    for(todo of todos) {
+        var todoElement = document.createElement("li");
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement("a");
+        var linkText = document.createTextNode("Excluir");
+
+        linkElement.appendChild(linkText);
+        linkElement.setAttribute("href", "#");
+ 
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+
+    }
+}
+
+renderTodos();
+
+function addTodo() { 
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = "";
+    renderTodos();
+}
+
+
+function deleteTodo(params) {
+    todos.splice(pos, 1);
+    renderTodos();
+}
